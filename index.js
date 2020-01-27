78014c4b6dde550283c0c6e12093391503930ef6
Vagus-art <agustin.favo@gmail.com>
2020-01-27 10:14:21 -0300

    submit uploads image to imgur and console logs the link to the recently uploaded file

--- index.js
+++ index.js
@@ -15,14 +15,42 @@
 });*/
 
 //this is curious, I must learn all this browser apis
-const onChange = (event) => {
-    var file = event.srcElement.files[0];      
-    var img = document.createElement("img");
-    var reader = new FileReader();
-    reader.onloadend = function() {
-            img.src = reader.result;
-    }
-    reader.readAsDataURL(file);
-    document.body.appendChild(img);
+const readImage = (file) =>{
+  const img = document.createElement("img");
+  const reader = new FileReader();
+  reader.onloadend = function() {
+          img.src = reader.result;
+  }
+  reader.readAsDataURL(file);
+  return img;
 }
 
+const onChange = (event) => {
+  /*
+    const file = event.srcElement.files[0];
+    const img = readImage(file);
+    const image = document.getElementById("image");
+    image.parentNode.replaceChild(img,image);*/
+}
+
+const submitFile = async (event) => {
+    event.preventDefault();
+    const file = event.target.elements.myfile.files[0];
+    console.log(file);
+    const response = await fetch('https://api.imgur.com/3/image',{
+      method:'post',
+      headers:{
+        'Authorization':'Client-ID 2d9eb78dab2f6f0'
+      },
+      body:file
+    });
+    const json = await response.json();
+    const id = json.data.id;
+    const responseimg = await fetch('https://api.imgur.com/3/image/'+id,{
+      headers:{
+        'Authorization':'Client-ID 2d9eb78dab2f6f0'
+      }
+    });
+    const responseimgjson = await responseimg.json()
+    console.log(responseimgjson.data.link);
+}
